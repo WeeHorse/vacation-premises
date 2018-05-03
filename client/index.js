77954ab81e6a5f426632308a2586afd05f2a114d
WeeHorse <benjaminberglund@gmail.com>
2018-05-03 15:26:02 +0200

    working on it

--- client/index.js
+++ client/index.js
@@ -1,6 +1,10 @@
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
+
 // config
 const http = axios;
 let customers = [];
+let owners = [];
+let apartments = [];
 
 $(start);
 async function start(){
@@ -10,21 +14,28 @@ async function start(){
   // check
   if(users.data){
 
-    // 2. make some customer objects
     for(let user of users.data){
+      // 2. make some customer objects
       if(user.type == 'Customer'){
         customers.push(new Customer(user.firstName, user.lastName));
       }
+      // 3. make some owner objects
+      if(user.type == 'Owner'){
+        owners.push(new Owner(user.firstName, user.lastName));
+      }
     }
     console.log('customers', customers);
-
-    // 3. make some owner objects
-
   }
 
   // 4. make some appartment objects
+  let apartments = await http.get('data/apartments');
+  if(apartments.data){
+    for(let apartment of apartments.data){
+      // instanciate
+    }
+  }
 
-  // 5. make a contract for an appartment rental between a customer and an owner
+  // 5. make a contract for an apartment rental between a customer and an owner
 
   // 6. add an agent to the contract
 
@@ -34,8 +45,3 @@ async function start(){
     // c) the contract is issued to the customer and owner by the agent
 
 }
-
-
-
-
-
